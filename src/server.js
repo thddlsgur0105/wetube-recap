006dccef74498de2d777aac0ddaeb8e968b8f1ed
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-04-20 17:16:37 +0900

    Handled root , /login URL

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+    return res.send("I'm responding you.");
+}
+
+const handleLogin = (req, res) => {
+    return res.send("Login here.")
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
