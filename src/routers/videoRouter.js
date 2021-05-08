d46551fc427a00d11b1e34f75d3dbed441b0ee24
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-05-08 23:03:32 +0900

    Done Editing Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { deleteVideo, edit, watch, upload } from "../controllers/videoController";
+import { deleteVideo, getEdit, watch, upload, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
