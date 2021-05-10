2bdf7ec108a330bd565aabb2c4943f88d55f3f1d
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-05-10 14:15:25 +0900

    Finished Upload Part

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -45,7 +45,25 @@ export const postEdit = (req, res) => {
     return res.redirect(`/videos/${id}`);
 };
 export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload Video");
+
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+    const { title } = req.body;
+    const newVideo = {
+        title,
+        rating: 0,
+        comments: 0,
+        createdAt: "2 minutes ago",
+        views: 0,
+        id: videos.length + 1,
+    }
+    videos.push(newVideo);
+    return res.redirect("/"); 
+}
+
 export const deleteVideo = (req, res) => {
     return res.send("Delete Video");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { deleteVideo, getEdit, watch, upload, postEdit } from "../controllers/videoController";
+import { deleteVideo, getEdit, watch, postEdit, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
