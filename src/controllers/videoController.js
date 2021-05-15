e75efbe5e53bdcbeea597d4dfceb23870c3a89ec
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-05-15 19:37:24 +0900

    Finished Edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -34,7 +34,7 @@ export const postEdit = async (req, res) => {
         return res.render("404", { pageTitle: "Video not found." });
     }
     await Video.findByIdAndUpdate(id, {
-        title, description, hashtags: hashtags.split(",").map((word) => word.startsWith("#") ? word : `#${word}`),
+        title, description, hashtags: Video.formatHashtags(hashtags),
     })
     return res.redirect(`/videos/${id}`);
 };
@@ -50,7 +50,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags,
+            hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/"); 
     } catch(error) {
