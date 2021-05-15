fe54ff500ff2cc2509add58c3f458e4411235bf3
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-05-15 17:17:47 +0900

    Updated Edit Part

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,9 +26,16 @@ export const getEdit = async (req, res) => {
     return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id: id });
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    await Video.findByIdAndUpdate(id, {
+        title, description, hashtags: hashtags.split(",").map((word) => word.startsWith("#") ? word : `#${word}`),
+    })
     return res.redirect(`/videos/${id}`);
 };
 export const search = (req, res) => res.send("Search");
@@ -43,7 +50,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map((word) => `#${word}`),
+            hashtags,
         });
         return res.redirect("/"); 
     } catch(error) {
