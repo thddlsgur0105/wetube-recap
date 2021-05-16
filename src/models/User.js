795fc93ab385090282149c3aa2a7cb3d693383eb
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-05-16 22:24:33 +0900

    Created User Join

---
+++ src/models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+mongoose.set('useCreateIndex', true);
+
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    location: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
