3cdb240a859f9fe3d38676a0e97887299cc58cb9
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-05-20 15:53:52 +0900

    Github Login Process

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -42,6 +42,22 @@ export const postLogin = async (req, res) => {
     req.session.user = user;
 
     res.redirect("/");
+};
+
+export const startGithubLogin = (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config = {
+        client_id: "aa4932d13731efac8316",
+        allow_signup: false,
+        scope: "read:user user:email",
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {
+
 }
 
 export const logout = (req, res) => res.send("Log Out");
