794c09247c7c2b45c3edddea0210ce93c43c3c92
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-05-21 15:28:12 +0900

    Getting user data using api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
@@ -65,14 +66,35 @@ export const finishGithubLogin = async (req, res) => {
     };
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`;
-    const data = await fetch(finalUrl, {
+    const tokenRequest = await (await fetch(finalUrl, {
         method: "POST",
         headers: {
             Accept: "application/json",
         }
-    });
-    const json = await data.json();
-    console.log(json)
+    })).json();
+    if ("access_token" in tokenRequest) {
+        const { access_token } = tokenRequest;
+        const apiUrl = "https://api.github.com";
+        const userData = await (await fetch(`${apiUrl}/user`, {
+            headers: {
+                Authorization: `token ${access_token}`,
+            }
+        })).json();
+        console.log("userData", userData);
+        const emailData = await (await fetch(`${apiUrl}/user/emails`, {
+            headers: {
+                Authorization: `token ${access_token}`,
+            }
+        })).json();
+        console.log("emailData", emailData);
+        const email = emailData.find(email => email.primary === true && email.verified === true)
+        if (!email) {
+            return res.redirect("/login");
+        }
+        return res.send("Done!")
+    } else {
+        return res.redirect("/login");
+    }
 }
 
 export const logout = (req, res) => res.send("Log Out");
