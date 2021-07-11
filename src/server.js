9ac78c385c84b47a813ccb69dc93f725382b3fff
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-07-11 15:02:11 +0900

    Video Upload

--- src/server.js
+++ src/server.js
@@ -24,6 +24,7 @@ app.use(session({
 
 app.use(localMiddleware);
 
+app.use("/uploads", express.static("uploads"))
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
