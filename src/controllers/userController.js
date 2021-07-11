85996a43199daf34da5db931a5b61895dc2c53d4
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-07-11 17:49:32 +0900

    User Profile Done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -177,5 +177,12 @@ export const postChangePassword = async (req, res) => {
     return res.redirect("/users/logout")
 }
 
-export const see = (req, res) => res.send("See User Profile");
+export const see = async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+        return res.status(404).render("404", { pageTitle: "User not found." })
+    }
+    return res.render("users/profile", { pageTitle: `${user.name}의 Profile`, user });
+};
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -9,6 +9,8 @@ userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUploa
 userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start",publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish",publicOnlyMiddleware, finishGithubLogin);
+
+// user profile router
 userRouter.get("/:id", see);
 
 export default userRouter;
