5b451601da30593b2c577ca76bd478419408d006
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-07-14 14:50:56 +0900

    Webpack Done

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,14 @@ const path = require("path");
 module.exports = {
     entry: "./src/client/js/main.js",
     mode: "development",
+    watch: true,
     plugins: [new MiniCssExtractPlugin({
         filename: "css/styles.css"
     })],
     output: {
         filename: "js/main.js",
         path: path.resolve(__dirname, "assets"),
+        clean: true,
     },
     module: {
         rules: [
