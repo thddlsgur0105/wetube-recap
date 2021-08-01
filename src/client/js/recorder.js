c341a7456049d9bbff04e084e871e1f15565cfc2
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-08-01 17:41:41 +0900

    Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,10 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-    console.log(stream);
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { width: 200, height: 100 }, audio: false });
+    video.srcObject = stream;
+    video.play();
 }
 
 startBtn.addEventListener("click", handleStart);
