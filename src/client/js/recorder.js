22b4a9d506c974ba61dbf8595a858a0a1e959e43
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-08-02 22:00:29 +0900

    Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,36 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: { width: 200, height: 100 }, audio: false });
+let stream;
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+}
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        console.log("Recording finished.")
+        console.log(e.data);
+    };
+    recorder.start();
+    setTimeout(() => {
+        recorder.stop();
+    }, 10000);
+}
+
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({ video: { width: 200, height: 100 }, audio: false });
     video.srcObject = stream;
     video.play();
 }
+
+init();
 
 startBtn.addEventListener("click", handleStart);
