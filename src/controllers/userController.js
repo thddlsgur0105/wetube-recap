2f740b354c3a0cef24266a97f885494e5145ca55
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-08-11 12:08:51 +0900

    isHeroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -50,9 +50,9 @@ export const postEdit = async (req, res) => {
             return res.status(400).render("edit-profile", { pageTitle: "Edit Profile", errorMessage: "This name/username/email/location is already taken." });
         }
     }
-
+    const isHeroku = process.env.NODE_ENV === "production";
     const updatedUser = await User.findByIdAndUpdate(_id, {
-        avatarUrl: file ? file.location : avatarUrl,
+        avatarUrl: file ? ( isHeroku ? file.location : file.path ) : avatarUrl,
         name,
         email,
         username,
